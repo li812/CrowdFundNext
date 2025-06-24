47c098b5ed296687735843d25d7321ecf0b608e7
Ali Ahammad <68907011+li812@users.noreply.github.com>
2025-06-24 19:53:20 +0530

    Update UserBase.jsx

--- client/src/pages/Users/UserBase.jsx
+++ client/src/pages/Users/UserBase.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Avatar, Box, Button, InputBase, Menu, MenuItem, Container } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Avatar, Box, InputBase, Menu, MenuItem, Container } from '@mui/material';
 import { Home, Settings, Logout, Search } from '@mui/icons-material';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
@@ -34,33 +34,34 @@ function UserBase() {
             <Search sx={{ color: '#1976d2', mr: 1 }} />
             <InputBase placeholder="Search campaigns, people..." sx={{ flex: 1 }} />
           </Box>
-          {/* Welcome Message */}
-          {user && (
-            <Typography variant="body1" sx={{ mr: 2 }}>
-              Welcome, <b>{user.firstName} {user.lastName}</b>
-            </Typography>
-          )}
-          {/* Avatar & Menu */}
-          <IconButton onClick={handleMenuOpen} sx={{ p: 0, ml: 1 }}>
-            <Avatar
-              alt={user?.firstName || user?.email}
-              src={user?.profilePicture ? `${import.meta.env.VITE_API_URL}${user.profilePicture}` : undefined}
-              sx={{ width: 44, height: 44, bgcolor: '#ff6b6b', color: 'white' }}
-            >
-              {user?.firstName ? user.firstName[0] : (user?.email ? user.email[0] : '')}
-            </Avatar>
-          </IconButton>
-          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-            <MenuItem onClick={() => { handleMenuClose(); navigate('/user'); }}>
-              <Home sx={{ mr: 1 }} /> Home
-            </MenuItem>
-            <MenuItem onClick={() => { handleMenuClose(); navigate('/user/settings'); }}>
-              <Settings sx={{ mr: 1 }} /> Settings
-            </MenuItem>
-            <MenuItem onClick={handleLogout}>
-              <Logout sx={{ mr: 1 }} /> Logout
-            </MenuItem>
-          </Menu>
+          {/* User Name & Avatar on right */}
+          <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto' }}>
+            {user && (
+              <Typography variant="body1" sx={{ mr: 2, fontWeight: 500 }}>
+                {user.firstName} {user.lastName}
+              </Typography>
+            )}
+            <IconButton onClick={handleMenuOpen} sx={{ p: 0 }}>
+              <Avatar
+                alt={user?.firstName || user?.email}
+                src={user?.profilePicture ? `${import.meta.env.VITE_API_URL}${user.profilePicture}` : undefined}
+                sx={{ width: 44, height: 44, bgcolor: '#ff6b6b', color: 'white' }}
+              >
+                {user?.firstName ? user.firstName[0] : (user?.email ? user.email[0] : '')}
+              </Avatar>
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+              <MenuItem onClick={() => { handleMenuClose(); navigate('/user'); }}>
+                <Home sx={{ mr: 1 }} /> Home
+              </MenuItem>
+              <MenuItem onClick={() => { handleMenuClose(); navigate('/user/settings'); }}>
+                <Settings sx={{ mr: 1 }} /> Settings
+              </MenuItem>
+              <MenuItem onClick={handleLogout}>
+                <Logout sx={{ mr: 1 }} /> Logout
+              </MenuItem>
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
       {/* Main Content */}
