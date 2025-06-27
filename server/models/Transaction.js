c85fa32a984ef67ae25980b945ca28b1189caa88
Ali Ahammad <68907011+li812@users.noreply.github.com>
2025-06-28 00:14:15 +0530

    Update Transaction.js

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const transactionSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: 'Users',
     required: true
   },
